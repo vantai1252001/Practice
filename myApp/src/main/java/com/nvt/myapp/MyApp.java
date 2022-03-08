3d3d8629e9ed8d489b3b46178bbf62b2e92c46bc
Van Tai <nguyenvantai200118@gmail.com>
2022-03-08 21:51:49 +0700

    update code for feature test

--- myApp/src/main/java/com/nvt/myapp/MyApp.java
+++ myApp/src/main/java/com/nvt/myapp/MyApp.java
@@ -14,5 +14,8 @@ public class MyApp {
     public static void main(String[] args) {
         System.out.println("Hello Worl");
         System.out.println("as");
+        int a = 3;
+        int b = 5;
+        System.out.println( a + b );
     }
 }
